af9165dda99fb7698d1aca77503763a47cd73d30
Gustavo Dias <gusdias3301@gmail.com>
2018-11-02 20:53:14 -0300

    Algumas classes criadas e tabuleiro sendo inicializado hard code

--- src/Board.py
+++ src/Board.py
@@ -1,7 +1,54 @@
 # -*- coding: utf8 -*-
 import numpy as np
+from Square  import Square
+
+global TP; 
+global TL;
+global DL;
+global DP;
+global STAR;
+
+TP   = 10;
+TL   = 11;
+DL   = 12;
+DP   = 13;
+STAR = 14;
 
 class Board(object):
 	""""""
 	def __init__(self,dimension=15):
-		self.matrix
+		self.dimension = dimension;
+		self.matrix    = np.array([Square() for i in xrange(self.dimension*self.dimension)]).reshape(self.dimension,self.dimension);
+
+	def LoadSquares(self):
+		self.matrix[0,0].atribute  = TP;
+		self.matrix[0,3].atribute  = DL;
+		self.matrix[0,7].atribute  = TP;
+		self.matrix[0,11].atribute = DL;
+		self.matrix[0,14].atribute = TP;
+		self.matrix[1,1].atribute  = DP;
+		self.matrix[1,5].atribute  = TL;
+		self.matrix[1,9].atribute  = TL;
+		self.matrix[1,13].atribute = DP;
+		self.matrix[2,2].atribute  = DP;
+		self.matrix[2,6].atribute  = DL;
+		self.matrix[2,8].atribute  = DL;
+		self.matrix[2,12].atribute = DP;
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute = TP
+		self.matrix[0,0].atribute  = TP;
+		
+
+def main():
+	b = Board()
+	print b.matrix
+
+if __name__ == '__main__':
+	main()
--- src/Square.py
+++ src/Square.py
@@ -1,7 +1,6 @@
 # -*- coding: utf8 -*-
 class Square(object):
 	"""Quadrados do tabuleiro"""
-	def __init__(self, coord,atribute):
-		self.coord    = coord; #Tupla x y
+	def __init__(self, coord=(),atribute=None):
 		self.atribute = atribute;
 		self.letter   = None;
